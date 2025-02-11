386981ea1683feb2afd9549f23b6278e612c201d
Douglas <dhidekim@gmail.com>
2025-02-11 18:47:16 -0300

    Fix prettier e lint

--- pizzashop-web/src/main.tsx
+++ pizzashop-web/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import './global.css'
 
-import { App } from "./App";
-import "./global.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById("root")!).render(
+import { App } from './App'
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
