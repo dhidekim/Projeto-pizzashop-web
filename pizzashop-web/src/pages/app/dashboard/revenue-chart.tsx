bb92b14781f3bc48ed688d4c5fe5b97e4c64e3e8
Douglas <dhidekim@gmail.com>
2025-02-19 10:09:03 -0300

    Páginas e componentes (UI) - Gráfico de produtos populares

--- pizzashop-web/src/pages/app/dashboard/revenue-chart.tsx
+++ pizzashop-web/src/pages/app/dashboard/revenue-chart.tsx
@@ -52,6 +52,7 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
             <Line
               stroke={colors.violet[500]}
               type="linear"
